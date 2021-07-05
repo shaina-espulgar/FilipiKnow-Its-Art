de892c1e265cab07accb84e6aad6a8d7daf07104
Khuille Bagares <khuillebagares@gmail.com>
2021-07-05 17:42:36 +0800

    RE:Added as Script for the name default generation.
    SEE LOG.txt

--- Assets/Game Functions/AVATAR/nameHandling.cs
+++ Assets/Game Functions/AVATAR/nameHandling.cs
@@ -1,8 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+
+//HOPE THAT IT WILL NOT REMOVED
 
 public class nameHandling : MonoBehaviour
 {
+    public string nameOfPlayer;
+    public string DEFAULT_NAME;
+    public Text playerNameShowUI;
+
+    public void nameDefaultRandomization()
+    {
+        string[] DEFAULT_NAMES_INDEX = { "Aron", "Rapiest", "Terrier", "Nosferatu" };
+        DEFAULT_NAME = DEFAULT_NAMES_INDEX[Random.Range(0, DEFAULT_NAMES_INDEX.Length - 1)];
+    }
+
+    void Start()
+    {
+        nameDefaultRandomization();
+        Debug.Log(DEFAULT_NAME + " is your name");
+    }
+
     
 }
