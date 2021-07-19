730cf9dd6ae01a20ddc9c3b4363076bf13f08db5
shaina-espulgar <72699933+shaina-espulgar@users.noreply.github.com>
2021-07-19 13:51:29 +0800

    Updated the About Us Function

--- Assets/Game Functions/PLAY/PlayGame.cs
+++ Assets/Game Functions/PLAY/PlayGame.cs
@@ -1,40 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
-using UnityEngine.Networking.Match;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
 
 public class PlayGame : MonoBehaviour
 {
-    [SerializeField] private NetworkManagerLobby networkLobby;
-
     public void ReturnToMainMenu()
     {
-        GameObject.Destroy(GameObject.Find("Network Manager"));
         SceneManager.LoadScene("MainMenu");
     }
 
     public void GoToAvatarChangeScene()
     {
         SceneManager.LoadScene("AvatarMenu");
-    }
-
-    public void HostServer()
-    {
-        networkLobby.StartHost();
-    }
-
-    // Disconnects the server if you are the host
-    public void StopServer()
-    {
-        networkLobby.StopHost();
-    }
-
-    public void StopClient()
-    {
-        networkLobby.StopClient();
     }
 }
